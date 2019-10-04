307e03b9edca54a21008f973b26d57ce31ae292d
podilaaditya <podila.aditya@gmail.com>
2019-10-04 14:56:38 +0530

    Logger New

--- CPP-study/concept_practice/Logger.h
+++ CPP-study/concept_practice/Logger.h
@@ -9,79 +9,67 @@
 #include <mutex>
 
 
-// To enable the File Logging set to 1
-#define FILE_PRINT 0
-/*  this defines number of file we would cycle with 
-*/
-#define MAX_FILES  5
-#define MAX_LINES 1000
-#define FILE_NAME_PREFIX LOG_
+// // To enable the File Logging set to 1
+// #define FILE_PRINT 0
+// /*  this defines number of file we would cycle with 
+// */
+// #define MAX_FILES  5
+// #define MAX_LINES 1000
+// #define FILE_NAME_PREFIX LOG_
 
-// === auxiliar functions
-static inline char *timenow();
+// // === auxiliar functions
+// static inline char *timenow();
 
-#define _FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
+// #define _FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
 
-#define NO_LOG          0x00
-#define ERROR_LEVEL     0x01
-#define INFO_LEVEL      0x02
-#define DEBUG_LEVEL     0x03
+// #define NO_LOG          0x00
+// #define ERROR_LEVEL     0x01
+// #define INFO_LEVEL      0x02
+// #define DEBUG_LEVEL     0x03
 
-#ifndef LOG_LEVEL
-#define LOG_LEVEL   DEBUG_LEVEL
-#endif
+// #ifndef LOG_LEVEL
+// #define LOG_LEVEL   DEBUG_LEVEL
+// #endif
 
-#if  FILE_PRINT
-#define PRINTTOFILE(format, ...) printToFile(format, __VA_ARGS__)
-#endif
+// #if  FILE_PRINT
+// #define PRINTTOFILE(format, ...) printToFile(format, __VA_ARGS__)
+// #endif
 
-#define PRINTFUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
+// #define PRINTFUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
 
-#define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
-#define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
+// #define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
+// #define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
 
-#define NEWLINE     "\n"
+// #define NEWLINE     "\n"
 
-#define ERROR_TAG   "ERROR"
-#define INFO_TAG    "INFO"
-#define DEBUG_TAG   "DEBUG"
+// #define ERROR_TAG   "ERROR"
+// #define INFO_TAG    "INFO"
+// #define DEBUG_TAG   "DEBUG"
 
-#if LOG_LEVEL >= DEBUG_LEVEL
-#define LOG_DEBUG(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## args)
-#else
-#define LOG_DEBUG(message, args...)
-#endif
+// #if LOG_LEVEL >= DEBUG_LEVEL
+// #define LOG_DEBUG(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## args)
+// #else
+// #define LOG_DEBUG(message, args...)
+// #endif
 
-#if LOG_LEVEL >= INFO_LEVEL
-#define LOG_INFO(message, args...)      PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
-#else
-#define LOG_INFO(message, args...)
-#endif
+// #if LOG_LEVEL >= INFO_LEVEL
+// #define LOG_INFO(message, args...)      PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
+// #else
+// #define LOG_INFO(message, args...)
+// #endif
 
-#if LOG_LEVEL >= ERROR_LEVEL
-#define LOG_ERROR(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
-#else
-#define LOG_ERROR(message, args...)
-#endif
+// #if LOG_LEVEL >= ERROR_LEVEL
+// #define LOG_ERROR(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+// #else
+// #define LOG_ERROR(message, args...)
+// #endif
 
-#if LOG_LEVEL >= NO_LOGS
-#define LOG_IF_ERROR(condition, message, args...) if (condition) PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
-#else
-#define LOG_IF_ERROR(condition, message, args...)
-#endif
+// #if LOG_LEVEL >= NO_LOGS
+// #define LOG_IF_ERROR(condition, message, args...) if (condition) PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+// #else
+// #define LOG_IF_ERROR(condition, message, args...)
+// #endif
 
-static inline char *timenow() {
-
-    static char buffer[64];
-    time_t rawtime;
-    struct tm *timeinfo;
-    
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    
-    strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", timeinfo);    
-    return buffer;
-}
 
 
 class Logger{
@@ -98,7 +86,7 @@ class Logger{
 		  char 	fileName[1024];
 		  int 	level;
 		  int 	quiet;
-		} ;
+		};
 
 		enum { 
 			LOG_TRACE,
@@ -113,6 +101,22 @@ class Logger{
 	private:
 		Logger();
 		Logger(const Logger& obj);
+
+		/*
+		
+		*/
+		static inline char *timenow() {
+
+		    static char buffer[64];
+		    time_t rawtime;
+		    struct tm *timeinfo;
+		    
+		    time(&rawtime);
+		    timeinfo = localtime(&rawtime);
+		    
+		    strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", timeinfo);    
+		    return buffer;
+		}
 
 	public:
 		
